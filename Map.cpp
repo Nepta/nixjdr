1a8c5f0641ca6d689913112540bd6d3aa91ac204
nepta <nepta23@gmail.com>
2014-05-01 01:08:24 +0200

    init windows of the map

--- Map.cpp
+++ Map.cpp
@@ -1,28 +1,45 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
+#include "Map.h"
+#include <utility>
 using namespace sf;
 
-int main(){
+Map::Map(std::string path){
 	RenderWindow window(VideoMode(800,600),"map");
-	window.setFramerateLimit(120);
-	Texture forest;
-	if(!forest.loadFromFile("resource/littleForest.png")){
-		std::cout << "unable to load littleForest.png" << std::endl;
-	}
+	window_ = &window;
 	
-	Sprite background(forest);
+}
+
+void Map::unlimitRender(){
 	
-	while(window.isOpen()){
-		Event event;
-		while(window.pollEvent(event)){
-			if(event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Key::Escape)){
-				window.close();
-			}
-		}
-		window.clear();
-		window.draw(background);
-		window.display();
-	}
+}
+
+int main(){
+	Map map("resource/littleForest.png");
+	map.unlimitRender();
  return 0;
 }
+
+//int main(){
+//	RenderWindow window(VideoMode(800,600),"map");
+//	window.setFramerateLimit(120);
+//	Texture forest;
+//	if(!forest.loadFromFile("resource/littleForest.png")){
+//		std::cout << "unable to load littleForest.png" << std::endl;
+//	}
+//	
+//	Sprite background(forest);
+//	
+//	while(window.isOpen()){
+//		Event event;
+//		while(window.pollEvent(event)){
+//			if(event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Key::Escape)){
+//				window.close();
+//			}
+//		}
+//		window.clear();
+//		window.draw(background);
+//		window.display();
+//	}
+// return 0;
+//}
--- Map.h
+++ Map.h
@@ -1,4 +1,10 @@
+#include <SFML/Graphics.hpp>
+
 class Map{
-	
+	sf::RenderWindow* window_;
+
+public:
+	Map(std::string);
+	void unlimitRender();
 };
 
