e6ad9e412479fce545ee6d5223b09b355c249a13
obside <obsiagae@gmail.com>
2014-05-01 14:10:24 +0200

    mdiArea (stackable windows), create map window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,6 @@
+#include <QString>
+#include <QFileDialog>
+#include <QMdiSubWindow>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +9,26 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMainWindow::showFullScreen();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionModify_Background_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Ouvrir un fichier", "/home", "Images (*.png *.xpm *.jpg)");
+
+    if (filename != NULL) {
+        QMdiSubWindow* subWindow = new QMdiSubWindow();
+        subWindow->setWindowFlags( Qt::CustomizeWindowHint | Qt::WindowCloseButtonHint );
+
+        // TODO send string to sfml app
+
+        ui->tableArea->addSubWindow(subWindow);
+        subWindow->setGeometry(0, 0, ui->tableArea->width()/2, ui->tableArea->height()/2);
+        subWindow->show();
+    }
 }
