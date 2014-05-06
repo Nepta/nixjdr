59e87b7ed0129080605484a9851137d9c09e70ea
nepta <nepta23@gmail.com>
2014-05-06 03:16:03 +0200

    ActionChooser documentation

--- src/Qt-sfml/QTSFML/ActionChooser.h
+++ src/Qt-sfml/QTSFML/ActionChooser.h
@@ -4,9 +4,20 @@
 #include <vector>
 #include "QTSFMLCanvas.h"
 #include "Action.h"
-
+/**
+ * @brief The ActionChooser class
+ * Action factory, create and initialize action according to its attribute
+ */
 class ActionChooser{
+	/**
+	 * @brief selectedSprite_
+	 * point to a selected sprite when user press mouse or is null
+	 */
 	sf::Sprite *selectedSprite_ = nullptr;
+	/**
+	 * @brief isMovementAnimation_
+	 * true if user is draging a sprite
+	 */
 	bool isMovementAnimation_ = false;
 
 private:
@@ -19,10 +30,37 @@ private:
 	};
 	
 public:
+	/**
+	 * @brief instance
+	 * @return the unique instance of ActionChooser factory
+	 */
 	static ActionChooser& instance();
+	/**
+	 * @brief choose
+	 * @param x x mouse position when click is released
+	 * @param y y mouse position when click is released
+	 * @return an action to perform
+	 */
 	Action& choose(int x, int y);
+	/**
+	 * @brief selectSprite
+	 * @param x x mouse position when click is released
+	 * @param y y mouse position when click is released
+	 * last clicked sprite
+	 */
 	void selectSprite(int x, int y);
+	/**
+	 * @brief selectAction
+	 * @param x mouse position when click is released
+	 * @param y mouse position when click is released
+	 * @return an action to perform
+	 * hold logic to select action to perform
+	 */
 	ActionType selectAction(int x, int y);
+	/**
+	 * @brief isMoving
+	 * @param state true if user is dragin a sprite
+	 */
 	void isMoving(bool state);
 };
 
