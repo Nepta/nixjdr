4de056b764ccfe8323660d680ad66d4bbcb415e3
Obside <obsiagae@gmail.com>
2014-05-10 22:45:55 +0200

    edit map background -> new map created instead (fix)

--- src/QTSFML/MapMdiSubwindow.cpp
+++ src/QTSFML/MapMdiSubwindow.cpp
@@ -8,18 +8,13 @@ const int MapMdiSubwindow::margin = 24;
 
 MapMdiSubwindow::MapMdiSubwindow(QString filename)
 {
-    QImage bg = QImage(filename);
     scrollArea_ = new QScrollArea;
 
     setWindowTitle("Map");
-    resize(bg.width(), bg.height() + margin);
     setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowCloseButtonHint); // hide min/max buttons
 
-    map_ = new Map(this, QPoint(0, margin), bg.size());
-    map_->setMap(filename);
-    map_->show();
-
-    scrollArea_->setWidget(map_);
+    map_ = NULL;
+    editMapBackgroud(filename);
     setWidget(scrollArea_);
 }
 
@@ -30,14 +25,14 @@ MapMdiSubwindow::~MapMdiSubwindow() {
 void MapMdiSubwindow::editMapBackgroud(QString filename) {
     QImage bg = QImage(filename);
 
-    //resize(bg.width(), bg.height() + margin);
+    resize(bg.width(), bg.height() + margin);
 
-    //delete map_;
-    //map_ = new Map(this, QPoint(0, margin), bg.size());
+    delete map_;
+    map_ = new Map(this, QPoint(0, margin), bg.size());
     map_->setMap(filename);
-    //map_->show();
+    map_->show();
 
-    //scrollArea_->setWidget(map_);
+    scrollArea_->setWidget(map_);
 }
 
 Map* MapMdiSubwindow::map() {
