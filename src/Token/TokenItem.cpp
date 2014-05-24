ad2c4030d2aa9676851b80644c0d76b314838917
nepta <nepta23@gmail.com>
2014-05-24 10:06:39 +0200

    added TokenItem Class

---
+++ src/Token/TokenItem.cpp
@@ -0,0 +1,5 @@
+#include "TokenItem.h"
+
+TokenItem::TokenItem()
+{
+}
---
+++ src/Token/TokenItem.h
@@ -0,0 +1,10 @@
+#ifndef TOKENITEM_H
+#define TOKENITEM_H
+
+class TokenItem
+{
+public:
+	TokenItem();
+};
+
+#endif // TOKENITEM_H
