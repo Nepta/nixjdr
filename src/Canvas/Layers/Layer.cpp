ca611c7286bdd0b5b145be376ac3669a7063e0cb
Obside <obsiagae@gmail.com>
2014-05-27 23:07:35 +0200

    fix incomplete comment

--- src/Canvas/Layers/Layer.cpp
+++ src/Canvas/Layers/Layer.cpp
@@ -3,9 +3,10 @@
 #include "Layer.h"
 
 /**
- * @brief Layer::boundingRect Reimplemented from QGraphicsObject. The bounding rect corresponds to
- * the scene rect when the layer is enable, otherwise the bounding rect is
- * @return
+ * @brief Layer::boundingRect Reimplemented from QGraphicsObject. The bounding rectangle corresponds
+ * to the scene rectangle when the layer is enabled. Otherwise the bounding rect is a 0 width/height
+ * rectangle to avoid intercepting mouse events when the layer is disabled.
+ * @return bounding rectangle
  */
 QRectF Layer::boundingRect() const {
     if (isEnabled()) {
