94e68d1de06e55ddd11b462644d421bb750a17d9
nepta <nepta23@gmail.com>
2014-06-04 13:49:36 +0200

    use log to log

--- src/Canvas/Network/MapServer.cpp
+++ src/Canvas/Network/MapServer.cpp
@@ -2,29 +2,16 @@
 #include "Chat/Message.h"
 #include "MapServer.h"
 #include <QDebug>
+#include "Log/MapLog.h"
 
 MapServer::MapServer(QHash<QString, User *> *usersList) : ServerReceiver(usersList) {}
 MapServer::~MapServer() {}
 
 void MapServer::processNewMessage(Header header, QByteArray& data) {
 	Message message(data);
-
-	switch(header.getCode()){
-		case 1:
-			qDebug() << "addSpriteAction(" + message.getString() + ")";
-			break;
-
-		case 2:
-			qDebug() << "removeSpriteAction(" + message.getString() + ")";
-			break;
-
-		case 3:
-			qDebug() << "removeAllFoWAction(" + message.getString() + ")";
-			break;
-
-		default:
-			break;
-	}
-
+	MapLog mapLog(*this);
+	mapLog.setMessage(message);
+	mapLog.setHeader(header);
+	qDebug() << mapLog.toString();
 	sendPacketToAll((quint16) TargetCode::MAP_CLIENT, header.getCode(), message);
 }
