5e275fecf978712389131ac14900ad6bfaa8c52c
nepta <nepta23@gmail.com>
2014-06-04 12:17:40 +0200

    cheap debug log

--- src/Canvas/Network/MapServer.cpp
+++ src/Canvas/Network/MapServer.cpp
@@ -1,14 +1,30 @@
 #include "Network/Switch.h"
-
 #include "Chat/Message.h"
-
 #include "MapServer.h"
+#include <QDebug>
 
 MapServer::MapServer(QHash<QString, User *> *usersList) : ServerReceiver(usersList) {}
 MapServer::~MapServer() {}
 
 void MapServer::processNewMessage(Header header, QByteArray& data) {
-    Message message(data);
+	Message message(data);
 
-    sendPacketToAll((quint16) TargetCode::MAP_CLIENT, header.getCode(), message);
+	switch(header.getCode()){
+		case 1:
+			qDebug() << "addSpriteAction(" + message.getString() + ")";
+			break;
+
+		case 2:
+			qDebug() << "removeSpriteAction(" + message.getString() + ")";
+			break;
+
+		case 3:
+			qDebug() << "removeAllFoWAction(" + message.getString() + ")";
+			break;
+
+		default:
+			break;
+	}
+
+	sendPacketToAll((quint16) TargetCode::MAP_CLIENT, header.getCode(), message);
 }
