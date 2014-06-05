6b81fdedb644c7e2cc29af2a5fa290f36fab135c
nepta <nepta23@gmail.com>
2014-06-05 15:34:14 +0200

    Logger is another switch connected to server that log after sending information

--- src/Log/Logger.cpp
+++ src/Log/Logger.cpp
@@ -1,4 +1,18 @@
 #include "Logger.h"
+#include <QDebug>
+#include "Chat/Message.h"
 
-Logger::Logger(){
+Logger::Logger(QHash<QString, User *> *usersList) : Receiver(usersList){
+}
+
+void Logger::insert(TargetCode targetCode, Receiver *receiver){
+	m_Nodes.insert(targetCode, receiver);
+}
+
+void Logger::processNewMessage(Header header, QByteArray &data){
+	switchNewMessage(header, data);
+	QString target(QString::number(header.getTarget()));
+	QString code(QString::number(header.getCode()));
+	Message message(data);
+	qDebug() << "message captured: [" + target + "|" + code + "|" + message.getString() + "]";
 }
--- src/Log/Logger.h
+++ src/Log/Logger.h
@@ -1,9 +1,16 @@
 #ifndef LOGGER_H
 #define LOGGER_H
+#include "Network/Switch.h"
+#include "Network/Receiver.h"
 
-class Logger{
+class Logger : public Switch, public Receiver{
+
 public:
-	Logger();
+	Logger(QHash<QString, User *> *usersList);
+	void insert(TargetCode targetCode, Receiver *receiver);
+
+public slots:
+	void processNewMessage(Header header, QByteArray &data);
 };
 
 #endif // LOGGER_H
