b67aeb29f9d0bf2c23cc1e91460c3f151661fb63
nepta <nepta23@gmail.com>
2014-06-05 16:31:34 +0200

    fix logger which can now log something else than MapLog plus added a logStack

--- src/Log/Logger.cpp
+++ src/Log/Logger.cpp
@@ -10,12 +10,33 @@ void Logger::insert(TargetCode targetCode, Receiver *receiver){
 	m_Nodes.insert(targetCode, receiver);
 }
 
+Log* Logger::pop(){
+	return logStack_.pop();
+}
+
+void Logger::push(Log *log){
+	logStack_.push(log);
+}
+
 void Logger::processNewMessage(Header header, QByteArray &data){
 	Receiver *receiver = switchNewMessage(header, data);
+	Log *log = nullptr;
 	if(receiver->isLoggable()){
-		MapLog log(*receiver);
-		log.setMessage(data);
-		log.setHeader(header);
-		qDebug() << log.toString();
+		TargetCode target = (TargetCode)header.getTarget();
+		switch(target){
+			case TargetCode::MAP_SERVER:
+				log = new MapLog(*receiver);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	if(log){
+		log->setMessage(data);
+		log->setHeader(header);
+		push(log);
+		qDebug() << log->toString();
 	}
 }
--- src/Log/Logger.h
+++ src/Log/Logger.h
@@ -2,12 +2,18 @@
 #define LOGGER_H
 #include "Network/Switch.h"
 #include "Network/Receiver.h"
+#include "Log.h"
+#include <QStack>
 
 class Logger : public Switch, public Receiver{
+
+	QStack<Log*> logStack_;
 
 public:
 	Logger(QHash<QString, User *> *usersList);
 	void insert(TargetCode targetCode, Receiver *receiver);
+	Log* pop();
+	void push(Log *log);
 
 public slots:
 	void processNewMessage(Header header, QByteArray &data);
